62599024e349d94b4c40311817b5bc944dc8ab01
Frank <841290355@qq.com>
2023-01-27 03:55:58 +0800

    fix: 错误处理补充

--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -2,6 +2,7 @@ const { getUserInfo } = require('../service/user.service')
 const {
   userFormateError,
   userAlreadyExited,
+  userRegisterError
 } = require('../constants/err.type')
 const userValidator = async (ctx, next) => {
   const { user_name, password } = ctx.request.body
@@ -16,8 +17,20 @@ const userValidator = async (ctx, next) => {
 const verifyUser = async (ctx, next) => {
   const { user_name } = ctx.request.body
   //合理性
-  if (getUserInfo({ user_name })) {
-    ctx.app.emit('error', userAlreadyExited, ctx)
+  // if (await getUserInfo({ user_name })) {
+  //   ctx.app.emit('error', userAlreadyExited, ctx)
+  //   return
+  // }
+  try {
+    const res = await getUserInfo({ user_name })
+    if (res) {
+      console.error('用户名已经存在', { user_name })
+      ctx.app.emit('error', userAlreadyExited, ctx)
+      return
+    }
+  } catch (err) {
+    console.error('获取用户信息错误', err)
+    ctx.app.emit('error', userRegisterError, ctx)
     return
   }
   await next()
